7bfd0bc2e5066a49bca581916638c6282ad02930
LucasCVF <lucasferreiracf14@gmail.com>
2024-11-20 17:09:31 -0300

    Initial commit

---
+++ src/Componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+const Banner = () => {
+    return(
+        <header className="banner">
+            <img src="/imagens/Banner.jpeg" />
+        </header>
+    )
+}
+
+export default Banner
---
+++ src/Componentes/Descricao/index.js
@@ -0,0 +1,12 @@
+import './Descricao.css'
+
+const Descricao = () => {
+    return (
+        <section className='descricao'>
+            <h1>Explore e adicione os seu animes preferidos com a ordem que desejar!</h1>
+        </section>
+        
+    )
+}
+
+export default Descricao
