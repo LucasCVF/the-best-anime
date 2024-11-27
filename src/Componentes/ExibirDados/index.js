3548f2a165d2a4d1c46e008dd11ba8b858de29bd
LucasCVF <lucasferreiracf14@gmail.com>
2024-11-27 11:26:06 -0300

    adjust display css

--- src/Componentes/ExibirDados/index.js
+++ src/Componentes/ExibirDados/index.js
@@ -4,12 +4,11 @@ function ExibirDados (props) {
 
     return(
         props.cadastro.map((cadastro, index) => (
-            <div key={index}>
+            <div className='exibir-dados' key={index}>
                 <p><strong>Nome:</strong> {cadastro.nome}</p>
                 <p><strong>Anime 1:</strong> {cadastro.anime1}</p>
                 <p><strong>Anime 2:</strong> {cadastro.anime2}</p>
                 <p><strong>Anime 3:</strong> {cadastro.anime3}</p>
-                <hr></hr>
             </div>
         ))
     )
